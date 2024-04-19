c9fc4480d3223e5410bfb0eb3a7d0b3b0cd5861f
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-19 00:56:15 -0300

    Add project files.

---
+++ LacosDeRepeticao/Program.cs
@@ -0,0 +1,37 @@
+﻿int n1 = 0, max = 0, contador = 0, aux = 0;
+
+Console.WriteLine("Digite um número:");
+n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um número incrementador");
+max = int.Parse(Console.ReadLine());
+
+aux = n1;
+
+Console.WriteLine("Aqui é o do");
+do
+{
+    Console.WriteLine(aux);
+    aux++;
+    contador++;
+   
+} while (contador <= max);
+
+Console.WriteLine("Aqui éo while");
+contador = 0;
+aux = n1;
+Console.WriteLine(aux);
+while (contador < max)
+{
+    aux++;
+    Console.WriteLine(aux);
+    contador++;
+}
+
+Console.WriteLine("Aqui é o for");
+aux = n1;
+for (contador = 0; contador <= max; contador++)
+{
+    Console.WriteLine(aux);
+    aux++;
+}
